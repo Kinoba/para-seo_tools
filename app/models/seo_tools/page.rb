3ec462f511a48eabc9d6ab571de883a3c0098443
Gabriel Cardoso <gcardoso.w@gmail.com>
2015-11-02 12:32:26 +0100

    added dependent destroy on meta tags in page model. Added log when destroying page

--- app/models/seo_tools/page.rb
+++ app/models/seo_tools/page.rb
@@ -1,7 +1,8 @@
 module SeoTools
   class Page < ActiveRecord::Base
     belongs_to :meta_tags, class_name: '::MetaTags::List',
-                           foreign_key: :meta_tags_list_id
+                           foreign_key: :meta_tags_list_id, 
+                           dependent: :destroy
 
     accepts_nested_attributes_for :meta_tags
 
--- lib/seo_tools/skeleton.rb
+++ lib/seo_tools/skeleton.rb
@@ -13,10 +13,24 @@ module SeoTools
       self.site = Skeleton::Site.new
       # Evaluate the configuration block
       site.instance_exec(&block)
+
       # Save all the pages to database
       site.pages.each do |page|
         page.model.save!
       end
+
+      # Delete pages not in skeleton
+      destroy_deleted_pages!
     end
+
+    def self.destroy_deleted_pages!
+      identifiers = site.pages.map(&:identifier)
+      pages = SeoTools::Page.where.not(identifier: identifiers)
+
+      if (count = pages.count) > 0
+        Rails.logger.info("Destroying #{ count } removed pages from Skeleton")
+        pages.destroy_all
+      end
+    end      
   end
 end
